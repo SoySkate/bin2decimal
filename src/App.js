cdf5e7fd0b1e2e3839504a39b36f8c0234cf7b10
Andrew <andreumartintorres.amt@gmail.com>
2023-01-17 17:54:04 +0100

    projectCommited

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
+const [inputValue, setInputValue] = useState('');
+const [result, setResult] = useState(null);
+
+const convertFunction=() =>{
+  const splitArray = inputValue.split([]).map((num, index)=> parseInt(num));
+  const poweredArray = splitArray.reverse().map((n, index)=>n*(Math.pow(2,index))); 
+  return setResult(poweredArray?.reduce((a, b)=>a+b,0));
+}
+
+
+
+
+return(
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl font-bold underline my-10">
+          Binary2Decimal</h1>
+        <p>This is a simple converter about binary numbers to decimal numbers</p>
+        <p>Write your binary number:</p>
+        <div>
+           <input className='my-10 text-black rounded-md' type='number' value={inputValue}  onChange={(event) =>{setInputValue(event.target.value)}}/>
+           <button className="bg-green-500 my-3 font-bold text-black rounded px-2" onClick={() => convertFunction()}>Convert</button>
+          {result && <p className='mb-10'>Binary number to decimal result is: {result}</p>}
+        </div>
       </header>
     </div>
   );
